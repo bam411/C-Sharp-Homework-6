00c5853b3c56e1bea37f760652cb61b3b805d4be
Petrushov Denis <bam411@yandex.ru>
2022-11-30 13:40:49 +0300

    exercise 41

---
+++ Exercise_41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Write($"Введите количество чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+
+void InputNumbers(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int searchPositive(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0) count += 1;
+    }
+    return count;
+}
+
+InputNumbers(size);
+
+Console.WriteLine(searchPositive(numbers));
